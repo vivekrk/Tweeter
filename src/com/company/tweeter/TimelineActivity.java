5740b9734daa52cf8ddb3774dc82ae633a6f7e0b
Vivek R K <vravindranath@gmail.com>
2011-12-26 01:00:50 +0530

    Added some new button images.

--- src/com/company/tweeter/TimelineActivity.java
+++ src/com/company/tweeter/TimelineActivity.java
@@ -23,8 +23,9 @@ import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,28 +41,26 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 	/** Called when the activity is first created. */
 
 	private Account account;
-	
+
 	private TweeterDbHelper dbHelper;
-	
+
 	private SimpleCursorAdapter adapter;
-	
+
 	private int activeFeed = TwitterAccount.TIMELINE;
 	private int lastPageFetched = 1;
-	
+
 	private Cursor data;
-	
+
 	private boolean isFetchingData = false;
-	
+
 	private ListView timelineList;
-	
-	private ImageButton showTweets;
-	private ImageButton showMentions;
-	private ImageButton newTweet;
-	
+
+	private RadioGroup twitterActions;
+
 	private View loading;
-	
+
 	private int lastItemIndex;
-	
+
 	public int getLastItemIndex() {
 		return lastItemIndex;
 	}
@@ -73,53 +72,53 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 
 	private boolean isScrolling = false;
 	private Paging newPages;
-	
-    public boolean isScrolling() {
+
+	public boolean isScrolling() {
 		return isScrolling;
 	}
 
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        dbHelper = new TweeterDbHelper(this);
-        dbHelper.getWritableDatabase();
-        
-//        dbHelper.eraseDb();
-        
-        account = AccountManager.getInstance().getAccount();
-        
-        newPages = new Paging(1);
-        
-        if(account.isUserLoggedIn(this)) {
-        	Log.d(Constants.TAG, "user is logged in");
-        	
-        	AccessToken token = account.getTokenFromPreferences();
-        	account.setAccessToken(token);
-        	
-        	setContentView(R.layout.timeline_layout);
-        	initializeUI();
-        	
-        	updateTimelineUI(TwitterAccount.TIMELINE);
-//        	new GetStatuses().execute(newPages);
-        	new PollForData().execute();
-        } else {
-        	try {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		dbHelper = new TweeterDbHelper(this);
+		dbHelper.getWritableDatabase();
+
+		//        dbHelper.eraseDb();
+
+		account = AccountManager.getInstance().getAccount();
+
+		newPages = new Paging(1);
+
+		if(account.isUserLoggedIn(this)) {
+			Log.d(Constants.TAG, "user is logged in");
+
+			AccessToken token = account.getTokenFromPreferences();
+			account.setAccessToken(token);
+
+			setContentView(R.layout.timeline_layout);
+			initializeUI();
+
+			updateTimelineUI(TwitterAccount.TIMELINE);
+			//        	new GetStatuses().execute(newPages);
+			new PollForData().execute();
+		} else {
+			try {
 				login();
 			} catch (TwitterException e) {
 				Log.d(Constants.TAG, "Login failed");
 			}
-        	Log.d(Constants.TAG, "user is not logged in");
-        }
-    }
-    
+			Log.d(Constants.TAG, "user is not logged in");
+		}
+	}
+
 	/**
-     * Updates the status messages in the ListView. 
-     * 
-     * The cursor data is set to the SimpleCursorAdapter and all the text field data is populated.
-     * ViewBinder object handles the setting of the image to the ImageView.
-     */
-    
+	 * Updates the status messages in the ListView. 
+	 * 
+	 * The cursor data is set to the SimpleCursorAdapter and all the text field data is populated.
+	 * ViewBinder object handles the setting of the image to the ImageView.
+	 */
+
 	private void updateTimelineUI(int timelineType) {
 		String selection = null;
 		adapter = null;
@@ -128,7 +127,7 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 			selection = Constants.TIMELINE + "=" + "'" + TwitterAccount.TIMELINE + "'";
 			data = dbHelper.query(Constants.TABLE_NAME, null, selection);
 			break;
-			
+
 		case TwitterAccount.MENTIONS:
 			selection = Constants.TIMELINE + "=" + "'" + TwitterAccount.MENTIONS + "'";
 			data = dbHelper.query(Constants.TABLE_NAME, null, selection);
@@ -137,13 +136,13 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 		default:
 			break;
 		}
-		
-		
+
+
 		if (data.moveToFirst()) {
 			adapter = new TimelineAdapter(this, R.layout.tweet_row, data, 
 					new String[] {Constants.CREATED_TIME, Constants.USERNAME, Constants.PROFILE_IMAGE, Constants.TWEET, Constants.RETWEETED_BY}, 
 					new int[] {R.id.time, R.id.username, R.id.userImageView, R.id.tweetMessage, R.id.retweetedBy});
-			
+
 			timelineList.setAdapter(adapter);
 		}
 	}
@@ -151,23 +150,41 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 	/**
 	 * Initializes all the UI elements.
 	 */
-	
+
 	private void initializeUI() {
-    	timelineList = (PullToRefreshListView) findViewById(R.id.tweetList);
-    	
-    	newTweet = (ImageButton) findViewById(R.id.newStatus);
-    	newTweet.setOnClickListener(this);
-    	
-    	showTweets = (ImageButton) findViewById(R.id.showTweets);
-    	showTweets.setOnClickListener(this);
-    	
-    	showMentions = (ImageButton) findViewById(R.id.showMentions);
-    	showMentions.setOnClickListener(this);
-    	
-    	timelineList.setOnScrollListener(this);
-    	timelineList.setOnItemClickListener(this);
-    	((PullToRefreshListView) timelineList).setOnRefreshListener(new OnRefreshListener() {
-			
+		timelineList = (PullToRefreshListView) findViewById(R.id.tweetList);
+
+		twitterActions = (RadioGroup) findViewById(R.id.twitter_actions);
+		twitterActions.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				switch (checkedId) {
+				case R.id.showTweets:
+					showTweets();
+					break;
+
+				case R.id.showMentions:
+					showMentions();
+					break;
+
+				case R.id.showLists:
+					Toast.makeText(getApplicationContext(), "Sorry!!! Lists are not implemented", Toast.LENGTH_SHORT).show();
+					break;
+
+				case R.id.showTags:
+					Toast.makeText(getApplicationContext(), "Sorry!!! Tags are not implemented", Toast.LENGTH_SHORT).show();
+					break;
+
+				default:
+					break;
+				}
+			}
+		});
+
+		timelineList.setOnScrollListener(this);
+		timelineList.setOnItemClickListener(this);
+		((PullToRefreshListView) timelineList).setOnRefreshListener(new OnRefreshListener() {
+
 			public void onRefresh() {
 				if(ImageDownloader.isNetworkConnected(TimelineActivity.this)) {
 					new GetStatuses().execute(newPages);
@@ -178,26 +195,42 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 				}
 			}
 		});
-    	
-    }
-	
+
+	}
+
+	private void showMentions() {
+		activeFeed = TwitterAccount.MENTIONS;
+		updateTimelineUI(TwitterAccount.MENTIONS);
+		if(!isFetchingData) {
+			new GetStatuses().execute(newPages);
+		}
+	}
+
+	private void showTweets() {
+		activeFeed = TwitterAccount.TIMELINE;
+		updateTimelineUI(TwitterAccount.TIMELINE);
+		if(!isFetchingData) {
+			new GetStatuses().execute(newPages);
+		}
+	}
+
 	class GetStatuses extends AsyncTask<Paging, Integer, List<Status>> {
 
 		@Override
 		protected List<twitter4j.Status> doInBackground(Paging... params) {
 			List<twitter4j.Status> newStatuses = null;
 			List<twitter4j.Status> newMentions = null;
-			
+
 			if(account instanceof TwitterAccount) {
 				try {
 					isFetchingData = true;
 					Log.d(Constants.TAG, "Inside GetStatuses AsyncTask");
-						newStatuses = ((TwitterAccount) account).getHomeTimeline(params[0]);
-						newMentions = ((TwitterAccount) account).getMentions(params[0]);
+					newStatuses = ((TwitterAccount) account).getHomeTimeline(params[0]);
+					newMentions = ((TwitterAccount) account).getMentions(params[0]);
 					for (twitter4j.Status status : newStatuses) {
 						dbHelper.addStatus(status, TwitterAccount.TIMELINE);
 					}
-					
+
 					for (twitter4j.Status status : newMentions) {
 						dbHelper.addStatus(status, TwitterAccount.MENTIONS);
 					}
@@ -205,13 +238,13 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 					Log.d(Constants.TAG, "Twitter exception");
 				}
 			}
-			
+
 			return newStatuses;
 		}
-		
+
 		@Override
 		protected void onPostExecute(List<twitter4j.Status> result) {
-			
+
 			if(adapter != null) {
 				data.requery();
 				if(!isScrolling()) {
@@ -219,14 +252,14 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 					timelineList.smoothScrollToPosition(getLastItemIndex());
 					Log.d(Constants.TAG, "Last index: " + getLastItemIndex());
 				}
-				
+
 			}
 			else {
 				switch (activeFeed) {
 				case TwitterAccount.TIMELINE:
 					updateTimelineUI(TwitterAccount.TIMELINE);
 					break;
-					
+
 				case TwitterAccount.MENTIONS:
 					updateTimelineUI(TwitterAccount.MENTIONS);
 					break;
@@ -234,104 +267,104 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 				default:
 					break;
 				}
-				
+
 			}
 			isFetchingData = false;
 			Log.d(Constants.TAG, "Fetching new statuses");
-			
+
 			timelineList.removeFooterView(loading);
-			
+
 			((PullToRefreshListView) timelineList).onRefreshComplete();
 			super.onPostExecute(result);
 		}
-		
+
 	}
-    
+
 	/**
 	 * Check if the user is logged in by checking if there is a preference key related to the 
 	 * AccessToken. If not, then a WebView is displayed and authentication is completed and AccessToken is set.
 	 * The AccessToken is then written to the Preferences.
 	 * @throws TwitterException 
 	 */
-	
-    private void login() throws TwitterException {
-    	WebView webView = new WebView(this);
-    	webView.getSettings().setJavaScriptEnabled(true);
-    	webView.setWebViewClient(new WebViewClient() {
-    		@Override
-    		public void onPageFinished(WebView view, String url) {
-    			super.onPageFinished(view, url);
-    			if(url.contains(Constants.CALLBACK_URL)) {
-    				Uri uri = Uri.parse(url);
-    				
-    				String oAuthVerifier = uri.getQueryParameter(Constants.OAUTH_VERIFIER);
-    				AccessToken token = null;
-    				try {
+
+	private void login() throws TwitterException {
+		WebView webView = new WebView(this);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				super.onPageFinished(view, url);
+				if(url.contains(Constants.CALLBACK_URL)) {
+					Uri uri = Uri.parse(url);
+
+					String oAuthVerifier = uri.getQueryParameter(Constants.OAUTH_VERIFIER);
+					AccessToken token = null;
+					try {
 						token = account.getAccessToken(oAuthVerifier);
 					} catch (TwitterException e) {
 						// TODO Auto-generated catch block
 						Log.d(Constants.TAG, "Failed to get Access Token");
 					}
-    				
-    				account.setAccessToken(token);
-    				account.writeTokenToPrefs(token);
-    				
-    				setContentView(R.layout.timeline_layout);
-    				initializeUI();
-    				
-    				new PollForData().execute();
-//    				new GetStatuses().execute(newPages);
-    				
-    			}
-    		}
-    		
-    		@Override
-    		public void onReceivedError(WebView view, int errorCode,
-    				String description, String failingUrl) {
-    			super.onReceivedError(view, errorCode, description, failingUrl);
-    			Toast.makeText(getApplicationContext(), description, Toast.LENGTH_LONG).show();
-    			Log.d(Constants.TAG, "onReceivedError: " + description);
-    		}
-    	});
 
-    	webView.loadUrl(account.getAuthenticationUrl());
-    	setContentView(webView);
-    }
-    
-    @Override
-    protected void onDestroy() {
-    	// TODO Auto-generated method stub
-    	super.onDestroy();
-    	if(data != null && !data.isClosed()) {
-    		data.close();
-    	}
-    	
-    	dbHelper.close();
-    }
+					account.setAccessToken(token);
+					account.writeTokenToPrefs(token);
+
+					setContentView(R.layout.timeline_layout);
+					initializeUI();
+
+					new PollForData().execute();
+					//    				new GetStatuses().execute(newPages);
+
+				}
+			}
+
+			@Override
+			public void onReceivedError(WebView view, int errorCode,
+					String description, String failingUrl) {
+				super.onReceivedError(view, errorCode, description, failingUrl);
+				Toast.makeText(getApplicationContext(), description, Toast.LENGTH_LONG).show();
+				Log.d(Constants.TAG, "onReceivedError: " + description);
+			}
+		});
+
+		webView.loadUrl(account.getAuthenticationUrl());
+		setContentView(webView);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		if(data != null && !data.isClosed()) {
+			data.close();
+		}
+
+		dbHelper.close();
+	}
 
 	public void onScroll(AbsListView view, int firstVisibleItem,
 			int visibleItemCount, int totalItemCount) {
-		
+
 		boolean loadMore = firstVisibleItem + visibleItemCount >= totalItemCount;
 		if(loadMore && !isFetchingData ) {
 			Log.d(Constants.TAG, "Loading more tweets");
 			isFetchingData = true;
 			Paging oldPages = new Paging(lastPageFetched);
 			new GetStatuses().execute(oldPages);
-			
+
 			showLoadingAnimation();
-			
+
 			Log.d(Constants.TAG, "Fetching page number: " + lastPageFetched);
 			lastPageFetched = lastPageFetched + 1;
 			setLastItemIndex(firstVisibleItem + 1);
 		}
-		
+
 	}
 
 	private void showLoadingAnimation() {
 		LayoutInflater inflater = getLayoutInflater();
 		loading = inflater.inflate(R.layout.loading_tweets, null);
-		
+
 		timelineList.addFooterView(loading);
 	}
 
@@ -346,38 +379,22 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.showTweets:
-			activeFeed = TwitterAccount.TIMELINE;
-			updateTimelineUI(TwitterAccount.TIMELINE);
-			if(!isFetchingData) {
-				new GetStatuses().execute(newPages);
-			}
-			break;
-			
-		case R.id.showMentions:
-			activeFeed = TwitterAccount.MENTIONS;
-			updateTimelineUI(TwitterAccount.MENTIONS);
-			if(!isFetchingData) {
-				new GetStatuses().execute(newPages);
-			}
-			break;
-			
 		case R.id.newStatus:
 			Intent intent = new Intent(this, NewTweetActivity.class);
 			startActivityForResult(intent, Constants.NEW_TWEET);
 			break;
-			
+
 		default:
 			break;
 		}
-		
+
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		if (requestCode == Constants.NEW_TWEET) {
-			
+
 			if (resultCode == RESULT_OK) {
 				Toast.makeText(getApplicationContext(), "RESULT_OK",
 						Toast.LENGTH_LONG).show();
@@ -389,7 +406,7 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 			}
 		}
 	}
-	
+
 	class PollForData extends AsyncTask<Void, Integer, Void> {
 
 		@Override
@@ -399,16 +416,16 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 			SystemClock.sleep(1800000);
 			return null;
 		}
-		
+
 		private void pollForNewData() {
 			if(!isFetchingData) {
 				SystemClock.sleep(180000);
 				new GetStatuses().execute(newPages);
 				isFetchingData = true;
 			}
-			
+
 		}
-		
+
 		@Override
 		protected void onPostExecute(Void result) {
 			Log.d(Constants.TAG, "Inside onPostExecute of PollData");
@@ -417,22 +434,22 @@ public class TimelineActivity extends Activity implements OnScrollListener, OnCl
 			super.onPostExecute(result);
 		}
 	}
-	
+
 
 	public void onItemClick(AdapterView<?> view, View v, int position, long id) {
 		Intent intent = new Intent(getApplicationContext(), TweetDetailsActivity.class);
 		String statusID = (String) timelineList.getAdapter().getItem(position);
-		
+
 		Bundle extras = new Bundle();
 		extras.putString(Constants.RETWEETED_BY, ((TextView) v.findViewById(R.id.retweetedBy)).getText().toString());
 		extras.putString(Constants.STATUS_ID, statusID);
 		extras.putString(Constants.USERNAME, ((TextView) v.findViewById(R.id.username)).getText().toString());
 		extras.putString(Constants.TWEET, ((TextView) v.findViewById(R.id.tweetMessage)).getText().toString());
 		extras.putString(Constants.CREATED_TIME, ((TextView) v.findViewById(R.id.time)).getText().toString());
-		
+
 		intent.putExtras(extras);
-		
+
 		startActivity(intent);
 	}
-    
+
 }
